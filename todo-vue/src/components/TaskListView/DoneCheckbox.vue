fb82920e9465cecb3698512664bff6f1ce0d3615
Denis Nutiu <denis.nutiu@gmail.com>
2023-04-19 19:57:43 +0300

    Use v-btn for task done

--- todo-vue/src/components/TaskListView/DoneCheckbox.vue
+++ todo-vue/src/components/TaskListView/DoneCheckbox.vue
@@ -1,13 +1,14 @@
 <template>
-  <v-checkbox-btn
-    :model-value="item.isDone"
-    @update:modelValue="handleTaskStatusUpdate"
-  ></v-checkbox-btn>
+  <v-btn :icon="icon" variant="text" @click="handleTaskStatusUpdate" />
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from "vue"
+import type { PropType } from "vue"
+import { defineComponent } from "vue"
 import type { Task } from "@/domain/task"
+
+const iconUnchecked = "mdi-checkbox-blank-circle-outline"
+const iconChecked = "mdi-checkbox-marked-circle-outline"
 
 export default defineComponent({
   name: "DoneCheckbox",
@@ -18,10 +19,15 @@ export default defineComponent({
       required: true
     }
   },
+  computed: {
+    icon() {
+      return this.item.isDone ? iconChecked : iconUnchecked
+    }
+  },
   methods: {
-    handleTaskStatusUpdate(status: any) {
+    handleTaskStatusUpdate() {
       let clonedItem = { ...this.item }
-      clonedItem.isDone = status
+      clonedItem.isDone = !clonedItem.isDone
       this.$emit("task:statusChanged", clonedItem)
     }
   }
